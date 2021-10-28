303a39ca4fcbad2e516675b2d5f191b6e1f245e8
Juan Mireles <juanmireles@Juans-MBP.attlocal.net>
2021-10-27 23:31:16 -0500

    app working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import React, { Component } from 'react';
+import ApiContext from './ApiContext';
+import config from './config';
+import { Card, Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends Component {
+
+  state = {
+    body: {},
+    image: '',
+    setup: '',
+    punchline: ''
+  }
+
+  render() {
+
+    const getJoke = () => {
+      axios.get(`${config.API_JOKES}`, { 'headers': { 'x-rapidapi-host': `${config.API_ENDPOINT}`, 'x-rapidapi-key': `${config.API_KEY}`}})
+        .then(res => {
+          console.log(res.data)
+          this.setState({
+            body: res.data.body
+          });
+          console.log(this.state.body)
+      }
+        )}
+
+    const value = {
+      image: this.state.body.image,
+      setup: this.state.body.setup,
+      punchline: this.state.body.punchline
+    }
+      return (
+        <ApiContext.Provider value={value}>
+          <Card className='text-center' bg='dark' text='light'>
+            <Card.Body>
+              <Card.Title>
+                {value.setup}
+              </Card.Title>
+              <Card.Title>{value.punchline}</Card.Title>
+              <Button className='text-center' variant="primary" onClick={getJoke}>Get Joke</Button>
+            </Card.Body>
+          </Card>
+        </ApiContext.Provider>
+    );
+  }
 }
-
-export default App;
---
+++ src/config.js
@@ -0,0 +1,7 @@
+const config = {
+    API_ENDPOINT: 'dad-jokes.p.rapidapi.com',
+    API_KEY: '2d22e8d570msh8a2f4ff972870a5p1039dcjsn3b58c48ca0c4',
+    API_JOKES: 'https://dad-jokes.p.rapidapi.com/random/joke/png'
+}
+
+export default config;
